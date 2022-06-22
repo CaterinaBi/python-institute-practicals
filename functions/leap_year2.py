965a31e0f9ae4b3c122616bf766d72ef6d3106d4
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-22 20:28:27 +0100

    Adds comments

--- functions/leap_year2.py
+++ functions/leap_year2.py
@@ -8,6 +8,10 @@ def is_year_leap(year):
     	else:
         	return False
 
+# defines days_in_month(year, month) function
+# the function takes two arguments: year, month
+# the function calculates the number of days in a given month and in a given year
+# the function returns month_length
 def days_in_month(year, month):
 months = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
     is_year_leap(year)
@@ -16,6 +20,7 @@ months = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12
     month_length = months[month]
     return month_length
 
+# tests
 test_years = [1900, 2000, 2016, 1987]
 test_months = [2, 2, 1, 11]
 test_results = [28, 29, 31, 30]
