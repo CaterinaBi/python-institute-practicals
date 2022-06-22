b85b1183f89249e9b9d3de6da82a871496efde14
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-22 19:46:45 +0100

    Adds instructions

---
+++ functions/leap_year2.py
@@ -0,0 +1,22 @@
+def is_year_leap(year):
+#
+# Your code from LAB 4.3.1.6.
+#
+
+def days_in_month(year, month):
+#
+# Write your new code here.
+#
+
+test_years = [1900, 2000, 2016, 1987]
+test_months = [2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
+for i in range(len(test_years)):
+	yr = test_years[i]
+	mo = test_months[i]
+	print(yr, mo, "->", end="")
+	result = days_in_month(yr, mo)
+	if result == test_results[i]:
+		print("OK")
+	else:
+		print("Failed")
