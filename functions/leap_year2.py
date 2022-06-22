7433cef50453788e4e84a1168bc3e987a24f2c5a
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-22 19:48:10 +0100

    Fixes indentation

--- functions/leap_year2.py
+++ functions/leap_year2.py
@@ -1,8 +1,8 @@
 def is_year_leap(year):
- if year%4 == 0 and year%100 != 0 or year%400 == 0:
-        return True
-    else:
-        return False
+	if year%4 == 0 and year%100 != 0 or year%400 == 0:
+        	return True
+    	else:
+        	return False
 
 def days_in_month(year, month):
 #
