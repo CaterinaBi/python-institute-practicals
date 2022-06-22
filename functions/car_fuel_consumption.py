e855ca46d4277e3d91a2ca0cdba0ad2090ef06d1
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-22 22:11:46 +0100

    Defines second function

--- functions/car_fuel_consumption.py
+++ functions/car_fuel_consumption.py
@@ -7,10 +7,11 @@ def liters_100km_to_miles_gallon(liters):
     return miles / gallons
 
 def miles_gallon_to_liters_100km(miles):
-#
-# Write your code here
-#
+    km100 = miles * 1609.344 / 1000 / 100
+    litres = 3.785411784
+    return litres / km100
 
+# tests
 print(liters_100km_to_miles_gallon(3.9))
 print(liters_100km_to_miles_gallon(7.5))
 print(liters_100km_to_miles_gallon(10.))
