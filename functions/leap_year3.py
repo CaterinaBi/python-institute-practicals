294797833f5de40b9e10dc7198e4dedeb10aeef1
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-22 20:29:34 +0100

    Creates file

---
+++ functions/leap_year3.py
@@ -0,0 +1,16 @@
+def is_year_leap(year):
+#
+# Your code from LAB 4.3.1.6.
+#
+
+def days_in_month(year, month):
+#
+# Your code from LAB 4.3.1.7.
+#
+
+def day_of_year(year, month, day):
+#
+# Write your new code here.
+#
+
+print(day_of_year(2000, 12, 31))
