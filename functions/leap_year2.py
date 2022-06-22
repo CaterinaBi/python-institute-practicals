60c43e91b549d414cf0a96890d9b2a728f0058b9
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-22 20:26:24 +0100

    Adds comments

--- functions/leap_year2.py
+++ functions/leap_year2.py
@@ -1,3 +1,7 @@
+# defines is_year_leap(year) function
+# the function takes one argument: year
+# the function calculates whether any given year is leap or not
+# the funtion returns True or False accordingly
 def is_year_leap(year):
 	if year%4 == 0 and year%100 != 0 or year%400 == 0:
         	return True
