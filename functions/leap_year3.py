027104342ac0f7e5c7bd9b3b0388bf539ae31e66
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-22 20:31:33 +0100

    Implements first two functions

--- functions/leap_year3.py
+++ functions/leap_year3.py
@@ -1,12 +1,24 @@
+# defines is_year_leap(year) function
+# the function takes one argument: year
+# the function calculates whether any given year is leap or not
+# the funtion returns True or False accordingly
 def is_year_leap(year):
-#
-# Your code from LAB 4.3.1.6.
-#
+	if year%4 == 0 and year%100 != 0 or year%400 == 0:
+        	return True
+    	else:
+        	return False
 
+# defines days_in_month(year, month) function
+# the function takes two arguments: year, month
+# the function calculates the number of days in a given month and in a given year
+# the function returns month_length
 def days_in_month(year, month):
-#
-# Your code from LAB 4.3.1.7.
-#
+months = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+    is_year_leap(year)
+    if is_year_leap(year) == True:
+        months[2] = 29
+    month_length = months[month]
+    return 
 
 def day_of_year(year, month, day):
 #
