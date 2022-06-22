79a604ced48312f3c7cf6933c156c0359eba996d
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-22 21:51:36 +0100

    Adds comments

--- functions/prime_checker.py
+++ functions/prime_checker.py
@@ -1,3 +1,6 @@
+# defines the function is_prime;
+# is_prime takes one argument (the value to check)
+# is_prime returns True if the argument is a prime number, and False otherwise
 def is_prime(num):
     for i in range(2, int(1 + num ** 0.5)):
         if num % i == 0:
