7e7ce2b541c72209fcacd3700b079d1bdf572441
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-22 21:46:18 +0100

    Adds comments

--- functions/leap_year3.py
+++ functions/leap_year3.py
@@ -20,6 +20,9 @@ months = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12
     month_length = months[month]
     return month_length
 
+# defines day_of_year(year, month, day) function
+# the function takes three arguments: year, month, day
+# the function calculates the exact day (1-366) of the year based on the inputted arguments
 def day_of_year(year, month, day):
 def day_of_year(year, month, day):
 	days = 0
