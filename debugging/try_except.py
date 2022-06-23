49676bace43d438c87a50e9a3ea50b3cc6b9208b
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-23 11:11:27 +0100

    Adds comments

--- debugging/try_except.py
+++ debugging/try_except.py
@@ -1,11 +1,17 @@
+# creates a while loop
 while True:
+# creates try-except
     try:
+        # code to test
         number = int(input("Enter an int number: "))
         print(5/number)
         break
+    # catches ValueError exception
     except ValueError:
         print("Wrong value.")
+    # catches ZeroDivisionError exception
     except ZeroDivisionError:
         print("Sorry. I cannot divide by zero.")
+    # catches other exceptions
     except:
         print("I don't know what to do...")
