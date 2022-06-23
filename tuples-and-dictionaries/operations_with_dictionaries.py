575e459e668fb4a3c7859f9d9694e912cfd386fe
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-23 10:27:41 +0100

    Update operations_with_dictionaries.py

--- tuples-and-dictionaries/operations_with_dictionaries.py
+++ tuples-and-dictionaries/operations_with_dictionaries.py
@@ -11,3 +11,7 @@ print(empty_dictionary)
 # print specific values by calling their key
 print(dictionary['cat'])
 print(phone_numbers['Suzy'])
+
+# uses for loop to return an iterable object consisting of all the keys gathered within the dictionary
+for key in dictionary.keys():
+    print(key, "->", dictionary[key])
