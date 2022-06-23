5f67c1cddc539e126ee93449c3d04b76899c1d08
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-23 09:51:42 +0100

    Restricts function's scope

--- functions/bmi_calculator.py
+++ functions/bmi_calculator.py
@@ -1,4 +1,8 @@
 # creates function with two parameters
 # the function calculates bmi based on a person's weight and height
+# the function returns None if weight and/or height are not in a plausible range
 def bmi(weight, height):
-    return weight / height ** 2
+    if height < 1.0 or height > 2.5 or \
+    weight < 20 or weight > 200:
+        return None
+    return weight / height ** 2 
