3a2e9c88f6ec94c1592f1aa5df6ccfca374d1819
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-23 10:40:59 +0100

    Update tuples_and_dictionaries_can_work_together.py

--- tuples-and-dictionaries/tuples_and_dictionaries_can_work_together.py
+++ tuples-and-dictionaries/tuples_and_dictionaries_can_work_together.py
@@ -1,5 +1,14 @@
+# create an empty dictionary for the input data
+# the student's name is used as a key
+# the associated scores will be stored in a tuple
 school_class = {}
 
+# creates loop
+# sets name as input
+# breaks loop if input is null
+# sets score as input
+# breaks loop if score is not in desired range
+# if the student's name is unknown to the dictionary, create a new entry
 while True:
     name = input("Enter the student's name: ")
     if name == '':
@@ -13,7 +22,11 @@ while True:
         school_class[name] += (score,)
     else:
         school_class[name] = (score,)
-        
+
+# iterate through the sorted students' names
+# initialize the data needed to evaluate the average (sum and counter)
+# iterate through the tuple, taking all the subsequent scores and updating the sum, together with the counter
+# evaluate and print the student's name and average score.
 for name in sorted(school_class.keys()):
     adding = 0
     counter = 0
