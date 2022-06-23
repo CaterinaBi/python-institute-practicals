998bf6e013f9fb27c9a258cf362727160e68a78d
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-23 10:25:18 +0100

    Update operations_with_dictionaries.py

--- tuples-and-dictionaries/operations_with_dictionaries.py
+++ tuples-and-dictionaries/operations_with_dictionaries.py
@@ -1,5 +1,9 @@
+# initialise three different dictionaries
 dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
 phone_numbers = {'boss' : 5551234567, 'Suzy' : 22657854310}
 empty_dictionary = {}
 
-# Print the values here.
+# print the values
+print(dictionary)
+print(phone_numbers)
+print(empty_dictionary)
