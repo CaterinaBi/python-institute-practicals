41ab020c88c49037e895f00fcf5cec5b4210b3f3
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-23 10:26:12 +0100

    Update operations_with_dictionaries.py

--- tuples-and-dictionaries/operations_with_dictionaries.py
+++ tuples-and-dictionaries/operations_with_dictionaries.py
@@ -3,7 +3,11 @@ dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
 phone_numbers = {'boss' : 5551234567, 'Suzy' : 22657854310}
 empty_dictionary = {}
 
-# print the values
+# print the whole dictionaries
 print(dictionary)
 print(phone_numbers)
 print(empty_dictionary)
+
+# print specific values by calling their key
+print(dictionary['cat'])
+print(phone_numbers['Suzy'])
